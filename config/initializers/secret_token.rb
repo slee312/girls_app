4aa5d5b4f826277dc9822b582bb31f74dd348af0
Sunyan <slee312@jhu.edu>
2013-05-24 13:36:15 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GirlsApp::Application.config.secret_token = '5b68c5788ef967ef067e8bac0067e2ee7dbd658a7477d69a52f03df0ae70c708f742e7f2028f0c6f097dba68db80ff051f574aa6e62f4e422427068de78d5519'
